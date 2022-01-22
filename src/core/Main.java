8a7fdfc537ff847e5e0011e6f74667b3ee13d1f4
A Lao <55960699+Eerohne@users.noreply.github.com>
2022-01-22 17:56:20 -0500

    Implemented RAM

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,8 +1,19 @@
 package core;
 
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        RAM mem = new RAM();
+
+        mem.storeValue((byte)10, 'x');
+        mem.storeAValue('x', 'y');
+
+        Util.printBinary(mem.getValue('x'));
+        Util.printBinary(mem.getValue((char)('y' + 1)));
+        Util.printBinary(mem.getAValue('y'));
+
     }
 }
